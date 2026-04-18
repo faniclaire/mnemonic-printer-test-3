448591ddb03e1ff5b417fb1e3fe1ede6870b6e79
faniclaire <fanny.lamontagne@gmail.com>
2026-04-17 21:10:19 -0400

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,16 @@
+// @ts-nocheck
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+// PASTE YOUR GIANT MERGED CODE FROM GOOGLE AI STUDIO HERE
+// (Make sure the part you paste includes the Zone4_Printer component)
+
+const rootElement = document.getElementById('root');
+if (rootElement) {
+  ReactDOM.createRoot(rootElement).render(
+    <React.StrictMode>
+       {/* Change 'App' below to whatever your main component name is */}
+      <App /> 
+    </React.StrictMode>
+  );
+}
