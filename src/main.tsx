9e5b06c97d01d260825302fc37545662316462e2
faniclaire <fanny.lamontagne@gmail.com>
2026-04-17 21:06:48 -0400

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,24 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { RouterProvider } from 'react-router-dom'
+import App from './App.tsx'
+import './index.css'
 
-import { router } from './routes'
-
-import './assets/styles/fontface.css'
-import './assets/styles/tailwind.css'
-
-const rootElement = document.getElementById('root')
-
-if (!rootElement) {
-  throw new Error(
-    "Root element not found. Check if it's in your index.html or if the id is correct."
-  )
-}
-
-// When you use Strict Mode, React renders each component twice to help you find unexpected side effects.
-// @ref: https://react.dev/blog/2022/03/08/react-18-upgrade-guide#react
-ReactDOM.createRoot(rootElement).render(
+ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
-  </React.StrictMode>
+    <App />
+  </React.StrictMode>,
 )
